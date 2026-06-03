5afe8fbbbbc57b5836fe9a9667859ee4d937d51e
Nikita Kadam <kadamnik68@gmail.com>
2026-06-03 12:10:58 +0530

    connected render backend

--- todo-application/src/Utils/AppConst.js
+++ todo-application/src/Utils/AppConst.js
@@ -1,6 +1,6 @@
 
 // const BASE_URL = "http://localhost:5000/api";
-const BASE_URL = "https://YOUR-RENDER-URL.onrender.com/api";
+const BASE_URL = "https://todo-backend.onrender.com/api";
 
 export const API_URLS = {
 
