bcdac3edd70973ca736219536a8b6b88d2954307
Nikita Kadam <kadamnik68@gmail.com>
2026-06-03 14:49:45 +0530

    update api url for production

--- todo-application/src/Utils/AppConst.js
+++ todo-application/src/Utils/AppConst.js
@@ -1,6 +1,7 @@
 
 // const BASE_URL = "http://localhost:5000/api";
-const BASE_URL = "https://todo-backend.onrender.com/api";
+// const BASE_URL = "https://todo-backend.onrender.com/api";
+const BASE_URL = "https://todo-backend1-2bvo.onrender.com/api";
 
 export const API_URLS = {
 
