e19189feff01054f0887f148a2e00372ad33e421
Nikita Kadam <kadamnik68@gmail.com>
2026-06-03 11:57:39 +0530

    updated backend api url

--- todo-application/src/Utils/AppConst.js
+++ todo-application/src/Utils/AppConst.js
@@ -1,5 +1,6 @@
 
-const BASE_URL = "http://localhost:5000/api";
+// const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://YOUR-RENDER-URL.onrender.com/api";
 
 export const API_URLS = {
 
